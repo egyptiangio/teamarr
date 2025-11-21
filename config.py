396764c2c3e948cf8b568641f535134aa59125f7
egyptiangio <minazaki@gmail.com>
2025-11-21 00:00:28 -0500

    Bump version to 0.2.0

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ Teamarr - Sports Team EPG Generator Configuration
 """
 
 # Application version - single source of truth
-VERSION = "0.1.0"
+VERSION = "0.2.0"
 
 # Application settings
 APP_NAME = "Teamarr"
